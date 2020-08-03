b045ff004f3726c400ea7a1f1456eb1325ea94ab
小东 <49069586+x-dr@users.noreply.github.com>
2020-08-03 14:03:16 +0800

    Update moe.py

--- moe.py
+++ moe.py
@@ -74,7 +74,7 @@ def AllDown(x):
     #print("全站共有%d页" % int(PageNum[0]))
     #for i in range(int(PageNum[0])):
     #12,21
-    for i in range(31,35):
+    for i in range(35,39):
         #i = i + 1
         if i == '0':
             PageUrl = "https://www.moestack.com/all"
